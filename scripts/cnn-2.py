b0a29ac94fb431b5257631ce40f851b33301a532
Bashar Naji <7585742+basharnaji@users.noreply.github.com>
2022-01-24 20:01:53 -0800

    Updated to work with Python 3.x (#1)

--- scripts/cnn-2.py
+++ scripts/cnn-2.py
@@ -1,4 +1,5 @@
 import sys
+import importlib
 import scrapelib
 from bs4 import BeautifulSoup
 from dateutil import parser
@@ -23,23 +24,24 @@ columns = ['url',
 
 
 def extract_transcript(html):
-    soup = BeautifulSoup(html)
+    soup = BeautifulSoup(html, "html.parser")
     cnnTransStoryHead = soup.find('p', {"class": "cnnTransStoryHead"})
-    #print cnnTransStoryHead.text
+    # print cnnTransStoryHead.text
     cnnTransSubHead = soup.find('p', {"class": "cnnTransSubHead"})
-    #print cnnTransSubHead.text
-    #print html
+    # print cnnTransSubHead.text
+    # print html
     #body = soup.find('p', {"class": "cnnBodyText"})
-    #print body.text
+    # print body.text
 
     for a in soup.find_all('p', {"class": "cnnBodyText"}):
         text = a.text
-        m = re.match(r"Aired (.*\s+\d{1,2},\s+\d{4}\s+-\s+\d{2}:\d{2}).*([A-Z]{2,3})$", text)
+        m = re.match(
+            r"Aired (.*\s+\d{1,2},\s+\d{4}\s+-\s+\d{2}:\d{2}).*([A-Z]{2,3})$", text)
         if m:
             date = m.group(1)
             tz = m.group(2)
             date = parser.parse(date)
-            print date, tz
+            print(date, tz)
         elif text.startswith("THIS IS A RUSH TRANSCRIPT."):
             pass
         else:
@@ -47,7 +49,7 @@ def extract_transcript(html):
 
     data = {}
     try:
-        print date
+        print(date)
 
         data['channel.name'] = 'WWW'
         data['program.name'] = cnnTransStoryHead.text
@@ -59,48 +61,52 @@ def extract_transcript(html):
         data['subhead'] = cnnTransSubHead.text
         data['text'] = content
     except Exception as e:
-        print e
+        print(e)
     return data
 
 
 if __name__ == "__main__":
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
+    importlib.reload(sys)
 
-    f = open("cnn.csv", "ab")
-    writer =csv.DictWriter(f, fieldnames=columns, dialect='excel')
+    # sys.setdefaultencoding("utf-8")
+    if (sys.stdout.encoding != 'utf-8'):
+        sys.stdout.encoding = 'utf-8'
+
+    f = open("cnn.csv", "a")
+    writer = csv.DictWriter(f, fieldnames=columns, dialect='excel')
 
     writer.writeheader()
 
     s = scrapelib.Scraper(requests_per_minute=60)
 
-    startdate = date(2002, 9, 17)
+    startdate = date(2014, 6, 18)
 
     # Will be throttled to 10 HTTP requests per minute
     while True:
-        print startdate.year, startdate.month, startdate.day
-        res = s.urlopen('http://transcripts.cnn.com/TRANSCRIPTS/%04d.%02d.%02d.html' %
-                        (startdate.year, startdate.month, startdate.day))
-        soup = BeautifulSoup(res)
-        print soup.title
+        print(startdate.year, startdate.month, startdate.day)
+        res = s.get('http://transcripts.cnn.com/TRANSCRIPTS/%04d.%02d.%02d.html' %
+                    (startdate.year, startdate.month, startdate.day))
+
+        soup = BeautifulSoup(res.text, "html.parser")
+        print(soup.title)
         for a in soup.find_all("div", {"class": "cnnTransDate"}):
-            print a
+            print(a)
             soup2 = a.find_next_siblings('div')[0]
-            #for b in soup2.find_all("div", {"class": "cnnSectBulletItems"}):
+            # for b in soup2.find_all("div", {"class": "cnnSectBulletItems"}):
             for link in soup2.find_all("a"):
                 url = 'http://transcripts.cnn.com' + link['href']
-                print url
+                print(url)
                 try:
-                    res2 = s.urlopen(url)
-                    data = extract_transcript(res2)
+                    res2 = s.get(url)
+                    data = extract_transcript(res2.text)
                     data['url'] = url
                     writer.writerow(data)
                 except Exception as e:
-                    print e
-                    print url, res2.response
+                    print(e)
+                    print(url, res2.response)
                 print("Inner Break")
-                #break
+                # break
             print("Outer Break")
-            #break
+            # break
         startdate += timedelta(days=1)
     f.close()
